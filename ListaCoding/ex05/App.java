e8f03dca3766877e2a708c0faf9a6e5d5e0745c6
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2025-09-21 16:37:52 -0300

    Novo exercicio

--- ListaCoding/ex05/App.java
+++ ListaCoding/ex05/App.java
@@ -1,5 +1,22 @@
 package ListaCoding.ex05;
 
 public class App {
+    public static void main(String[] args) {
+        System.out.println("--- Demonstração TV 1 (Sala) ---");
+        TelevisaoSimples tvSala = new TelevisaoSimples();
+        tvSala.ligarDesligar();
+        tvSala.trocarCanal(10);
+        tvSala.aumentarVolume();
+        tvSala.aumentarVolume();
+        tvSala.mostrarStatus();
 
+        System.out.println("\n--- Demonstração TV 2 (Quarto) ---");
+        TelevisaoSimples tvQuarto = new TelevisaoSimples();
+        tvQuarto.ligarDesligar();
+        tvQuarto.trocarCanal(5);
+        for (int i = 0; i < 5; i++) {
+            tvQuarto.diminuirVolume();
+        }
+        tvQuarto.mostrarStatus();
+    }
 }
--- ListaCoding/ex05/TelevisaoSimples.java
+++ ListaCoding/ex05/TelevisaoSimples.java
@@ -1,5 +1,45 @@
 package ListaCoding.ex05;
 
 public class TelevisaoSimples {
+    // Exemplo 5: TelevisaoSimples
 
+    private boolean ligada = false;
+    private int canal = 1;
+    private int volume = 10;
+
+    public void ligarDesligar() {
+        this.ligada = !this.ligada;
+        System.out.println("TV " + (ligada ? "ligada." : "desligada."));
+    }
+
+    public void trocarCanal(int novoCanal) {
+        if (ligada && novoCanal > 0) {
+            this.canal = novoCanal;
+            System.out.println("Canal alterado para: " + this.canal);
+        } else {
+            System.out.println("A TV está desligada ou o canal é inválido.");
+        }
+    }
+
+    public void aumentarVolume() {
+        if (ligada && volume < 100) {
+            this.volume++;
+            System.out.println("Volume: " + this.volume);
+        }
+    }
+
+    public void diminuirVolume() {
+        if (ligada && volume > 0) {
+            this.volume--;
+            System.out.println("Volume: " + this.volume);
+        }
+    }
+
+    public void mostrarStatus() {
+        if (ligada) {
+            System.out.println("Status: Ligada | Canal: " + canal + " | Volume: " + volume);
+        } else {
+            System.out.println("Status: Desligada");
+        }
+    }
 }

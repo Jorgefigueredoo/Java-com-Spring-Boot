c3a36eb24299070dac3cc1a75c1aac81a6105b90
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2025-09-25 12:04:01 -0300

    Ätualizações

--- ListaCoding/ex20/App.java
+++ ListaCoding/ex20/App.java
@@ -2,4 +2,23 @@ package ListaCoding.ex20;
 
 public class App {
 
+    public static void main(String[] args) {
+        System.out.println("--- Demonstração: Liquidificador ---");
+
+        Liquidificador meuLiquidificador = new Liquidificador();
+
+        System.out.println("Tentando bater com o liquidificador desligado:");
+        meuLiquidificador.bater("banana e mamão");
+
+        System.out.println("\nAgora, ligando e batendo:");
+        meuLiquidificador.ligar();
+        meuLiquidificador.bater("banana e mamão");
+
+        System.out.println("\nAumentando a velocidade:");
+        meuLiquidificador.definirVelocidade(3);
+        meuLiquidificador.bater("gelo");
+
+        System.out.println("\nFinalizando:");
+        meuLiquidificador.desligar();
+    }
 }
